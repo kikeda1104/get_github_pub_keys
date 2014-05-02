bdcbd9345500a96b7410494f75cf5a79e061bcd4
Kazuyuki Ikeda <gankai1104@gmail.com>
2014-05-02 11:38:27 +0900

    .sshディレクトリの存在チェックと作成メソッドを追加

---
+++ lib/get_github_pub_keys/files/access.rb
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+module GetGithubPubKeys
+  module Files
+    class Access
+      DEFAULT_SSH_DIRECTORY = ".ssh"
+      attr_accessor :path
+
+      def initialize
+        @path = DEFAULT_SSH_DIRECTORY
+      end
+      # This method confirm exists .ssh directory. 
+      def ssh_exists?
+        File.exists(@path)
+      end
+      
+      # create /HOME_DIRECOTY/.ssh directory
+      def ssh_create
+        Dir.mkdir("#{env["HOME"]}/#{@path}")
+      end
+    end
+  end
+end
+
