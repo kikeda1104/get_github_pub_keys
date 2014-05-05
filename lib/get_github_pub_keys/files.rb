e08e5b53c61d8b3e78918871ffd3c0a4030f45f6
kazuyuki ikeda <gankai1104@gmail.com>
2014-05-05 21:03:34 +0900

    files周りを更新

--- lib/get_github_pub_keys/files.rb
+++ lib/get_github_pub_keys/files.rb
@@ -3,26 +3,27 @@ require 'logger'
 
 module GetGithubPubKeys
   module Files
-    DEFAULT_SSH_DIRECTORY = ".ssh"
+    DEFAULT_SSH_DIRECTORY = ".ssh".freeze
+    DEFAULT_IDENTIFY = '.pub'.freeze
     attr_accessor :path
-
-    def initialize
-      @path = DEFAULT_SSH_DIRECTORY
-    end
+    #
     # This method confirm exists .ssh directory. 
     def ssh_exists?
-      File.exists(@path)
+      File.exists(DEFAULT_SSH_DIRECTORY)
     end
     #
     # create /HOME_DIRECOTY/.ssh directory
     def ssh_create
-      Dir.mkdir("#{env["HOME"]}/#{@path}")
+      Dir.mkdir("#{env["HOME"]}/#{DEFAULT_SSH_DIRECTORY}")
     end
 
     # public_keys file create to .ssh directory.
-    def self.create( file_name, body )
-      file_name = file_name.to_s + Time.now.strftime("%Y%0m%0d")
-      File.write(@path + '/' + file_name.to_s, body)
+    def self.create(file_name, body)
+      file_name = file_name.to_s
+      file_name = File.basename(file_name)
+      # FIXME: 保存先がファイル実行ディレクトリになっている
+      file_name = file_name + "_" + Time.now.strftime("%Y%0m%0d%0H%0M%0S") + DEFAULT_IDENTIFY
+      File.open(file_name, "w") {|file| file.write(body)}
     rescue => e
       puts e.message
       nil
