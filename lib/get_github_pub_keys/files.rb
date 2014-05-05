344c5c862990f28442e74d6fdadfe10a46dac651
kazuyuki ikeda <gankai1104@gmail.com>
2014-05-05 21:40:32 +0900

    微修正

--- lib/get_github_pub_keys/files.rb
+++ lib/get_github_pub_keys/files.rb
@@ -3,7 +3,7 @@ require 'logger'
 
 module GetGithubPubKeys
   module Files
-    DEFAULT_SSH_DIRECTORY = ".ssh".freeze
+    DEFAULT_SSH_DIRECTORY = "/#{ENV["HOME"]}/.ssh".freeze
     DEFAULT_IDENTIFY = '.pub'.freeze
     attr_accessor :path
     #
@@ -14,16 +14,18 @@ module GetGithubPubKeys
     #
     # create /HOME_DIRECOTY/.ssh directory
     def ssh_create
-      Dir.mkdir("#{env["HOME"]}/#{DEFAULT_SSH_DIRECTORY}")
+      Dir.mkdir("#{ENV["HOME"]}/#{DEFAULT_SSH_DIRECTORY}")
     end
 
     # public_keys file create to .ssh directory.
+    # FIXME: githubのapiから手に入れる公開鍵のフォーマットについて調べる。
+    # 恐らく文字列のまま文字コードだけ気にしていれば良さそう
     def self.create(file_name, body)
       file_name = file_name.to_s
       file_name = File.basename(file_name)
       # FIXME: 保存先がファイル実行ディレクトリになっている
       file_name = file_name + "_" + Time.now.strftime("%Y%0m%0d%0H%0M%0S") + DEFAULT_IDENTIFY
-      File.open(file_name, "w") {|file| file.write(body)}
+      open(DEFAULT_SSH_DIRECTORY + "/" + file_name, "w:eucJP") {|file| file.write(body)}
     rescue => e
       puts e.message
       nil
