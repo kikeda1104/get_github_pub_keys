313fcc01eceeb2c984106f1a7ef0923ec936f391
Kazuyuki Ikeda <gankai1104@gmail.com>
2014-05-07 10:34:18 +0900

    余計なファイルとコードを削除しました

--- lib/get_github_pub_keys/files.rb
+++ lib/get_github_pub_keys/files.rb
@@ -1,24 +1,10 @@
 # coding: utf-8
-require 'logger'
-
 # FIXME: 定数値を別のファイルにまとめたい
 
 module GetGithubPubKeys
   module Files
     DEFAULT_SSH_DIRECTORY = "/#{ENV["HOME"]}/.ssh".freeze
     DEFAULT_IDENTIFY = '.pub'.freeze
-    attr_accessor :path
-    
-    # This method confirm exists .ssh directory. 
-    def ssh_exists?
-      File.exists(DEFAULT_SSH_DIRECTORY)
-    end
-    #
-    # create /HOME_DIRECOTY/.ssh directory
-    def ssh_create
-      Dir.mkdir("#{ENV["HOME"]}/#{DEFAULT_SSH_DIRECTORY}")
-    end
-
     # public_keys file create to .ssh directory.
     def self.create(file_name, body)
       file_name = file_name.to_s
