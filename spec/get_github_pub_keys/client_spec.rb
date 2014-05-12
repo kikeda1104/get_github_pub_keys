43c3ccc05680d48ec5cd3c8df6582f7f61f4a85a
kazuyuki ikeda <gankai1104@gmail.com>
2014-05-13 01:29:20 +0900

    client_specを追加

--- spec/get_github_pub_keys/client_spec.rb
+++ spec/get_github_pub_keys/client_spec.rb
@@ -4,4 +4,22 @@ require 'spec_helper'
 require 'get_github_pub_keys'
 
 describe GetGithubPubKeys::Client do
+  before do
+    let(:client) { Client.new }
+  end
+
+  describe "Client#new" do
+    it "is success" do
+      client = GetGithubPubKeys::Client.new("hoge")
+      expect(client).to be_a_kind_of(GetGithubPubKeys::Client)
+    end
+  end
+
+  describe 'Client#find_and_create' do
+    # expect
+    it "is there find_and_create" do
+      client = GetGithubPubKeys::Client.new("hoge")
+      expect(client.respond_to?("find_and_create").to be_nil
+    end
+  end
 end
