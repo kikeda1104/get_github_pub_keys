38d7e7c02f303e5a97cd04d67cfe409887aa8274
Kazuyuki.Ikeda <ikeda.kazuyuki@bpsinc.jp>
2014-05-15 18:12:22 +0900

    テストを修正

--- spec/get_github_pub_keys/client_spec.rb
+++ spec/get_github_pub_keys/client_spec.rb
@@ -5,19 +5,21 @@ require 'get_github_pub_keys'
 
 describe GetGithubPubKeys::Client do
   describe "Client#new" do
-    it "is success" do
-      client = GetGithubPubKeys::Client.new("hoge")
-      expect(client).to be_a_kind_of(GetGithubPubKeys::Client)
-    end
+    context "when GetGithubPubKeys::Client class" do
+      it "set username" do
+        client = GetGithubPubKeys::Client.new("hoge")
+        expect(client).to be_a_kind_of(GetGithubPubKeys::Client)
+      end
 
-    it "is failuer" do
-      client = GetGithubPubKeys::Client.new()
-      expect(client).to be_a_kind_of(GetGithubPubKeys::Client)
+      it "don't set username" do
+        client = GetGithubPubKeys::Client.new()
+        expect(client).to be_a_kind_of(GetGithubPubKeys::Client)
+      end
     end
   end
 
   describe 'Client#find_and_create' do
-    it "is there find_and_create" do
+    it "when find find_and_create" do
       client = GetGithubPubKeys::Client.new("hoge")
       expect(client.respond_to?("find_and_create")).to be_true
     end
